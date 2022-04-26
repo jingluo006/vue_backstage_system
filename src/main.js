fe81c5c82d520b763c983e5606b6086605c473c3
xhan <2396670743@qq.com>
2022-04-26 20:24:15 +0800

    数据统计图开发完成

--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
-
 Vue.use(VueQuillEditor)
 
 Vue.component('tree-table', treeTable)
--- src/views/Reports/Reports.vue
+++ src/views/Reports/Reports.vue
@@ -1,12 +1,79 @@
 <template>
   <div class="reportsContainer">
-    reports组件
+    <!-- 头部面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home/users' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片区域 -->
+    <el-card class="box-card">
+      <!-- echats图表 -->
+      <div id="report" style="width: 00px; height: 400px"></div>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+// 导入axios
+import axios from '@/utils/requestLogin.js'
+// 导入lodash
+import _ from 'lodash'
+// 导入echarts
+import * as echarts from 'echarts'
+export default {
+  data() {
+    return {
+      reportObj: {},
+      // 需要合并的数据
+      mergeOptions: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  async mounted() {
+    await this.getreportList()
+    var myChart = echarts.init(document.getElementById('report'))
+
+    myChart.setOption(this.reportObj)
+  },
+  methods: {
+    async getreportList() {
+      const { data: res } = await axios.get('reports/type/1')
+      if (res.meta.status !== 200) return this.$message.error('数据统计信息获取失败')
+      this.reportObj = _.merge(res.data, this.mergeOptions)
+      console.log(this.reportObj)
+    }
+  }
+}
 </script>
 
-<style>
-</style>
+<style></style>
